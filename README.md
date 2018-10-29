a58de97a097c77c058e96a37dd067162d6e5af1e
Lukasz Wojciech Kaleta <lka@novicell.co.uk>
2018-10-29 13:08:34 +0000

    Added package files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# CopyPasteGridEditors
+# Copy Paste Grid Editors (7.11+)
+
+Original package created by Søren Kottal (http://skttl.dk).
+ 
+This is a fixed version that works with Umbraco 7.11+
+ 
+Link to the original package: https://our.umbraco.com/packages/backoffice-extensions/copy-paste-grid-editors/
+
+This package adds copy/paste functionality to the grid editor.
+ 
+It works by hijacking the loading of the grid editor, and replaces it with a new one including the copy/paste functionality.
+ 
+When you copy a grid element, the element is saved in the browsers local storage. You can then paste it wherever the grid element is allowed.
+ 
+The package saves the last 10 copies, so you can have more elements copied.
+ 
+This package is also made as a pull request for Umbraco, so hopefully one day, it will be obsolete :)
+ 
+Thanks to Lars Erik Aabech for the initial pull request!
---
+++ copypastegrideditors.js
@@ -0,0 +1,91 @@
+﻿angular.module('umbraco').controller('Our.Umbraco.CopyPasteGridEditors',
+	['$scope', '$controller', 'localStorageService', '$element',
+		function ($scope, $controller, localStorageService, $element) {
+
+			var gridClipboardAlias = "grid-clipboard";
+
+			angular.extend(this, $controller('Umbraco.PropertyEditors.GridController', { $scope: $scope, $element: $element }));
+
+			$scope.copy = function (control) {
+				if (!Array.isArray(localStorageService.get(gridClipboardAlias))) {
+					localStorageService.set(gridClipboardAlias, []);
+				}
+
+				var newClipboard = localStorageService.get(gridClipboardAlias);
+
+				newClipboard.push(control);
+
+				if (newClipboard.length > 10) {
+					newClipboard.splice(0, newClipboard.length - 10);
+				}
+
+				localStorageService.set(gridClipboardAlias, newClipboard);
+
+				setClipboard();
+			}
+
+			var setClipboard = function () {
+				$scope.clipboard = localStorageService.get(gridClipboardAlias);
+				if (!Array.isArray($scope.clipboard)) {
+					$scope.clipboard = [];
+				}
+			}
+
+			setClipboard();
+
+			$scope.allowedEditorsInClipboard = function (cell) {
+				var allowedEditors = cell.$allowedEditors.map(function (editor) { return editor.alias; });
+				return $scope.clipboard.filter(function (control) {
+					return allowedEditors.indexOf(control.editor.alias) > -1;
+				});
+			}
+
+			$scope.paste = function (event, cell, index, copied) {
+				if (typeof (copied) == "undefined") {
+					// should be first allowed editor
+					var allowedEditors = $scope.allowedEditorsInClipboard(cell);
+					if (allowedEditors.length == 0) {
+						return false;
+					}
+					else {
+						copied = allowedEditors[allowedEditors.length - 1];
+					}
+				}
+
+				var newControl = $.extend({}, copied);
+				if (copied && copied.editor) {
+
+					if (index === undefined) {
+						index = cell.controls.length;
+					}
+
+					newControl.active = true;
+					$scope.initControl(newControl, index + 1);
+					cell.controls.push(newControl);
+				}
+			}
+
+
+			console.log("copypaste");
+		}]);
+
+console.log("cpge");
+
+angular.module('umbraco.services').config([
+	'$httpProvider',
+	function ($httpProvider) {
+
+		$httpProvider.interceptors.push(function ($q) {
+			return {
+				'request': function (request) {
+
+					// Redirect any requests for the help page, to team.ecreo.dk/support
+					if (request.url.startsWith("views/propertyeditors/grid/grid.html"))
+						request.url = "../App_Plugins/CopyPasteGridEditors/copypastegrid.html";
+
+					return request || $q.when(request);
+				}
+			};
+		});
+
+	}]);
